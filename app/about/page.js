d8d27a2cdaf05f70a77c43f01e48262038df1790
Mike Laurel <mike.laurel@niceforyou.com>
2024-04-19 13:01:00 -0700

    Adds about page and test tailwind

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function About() {
     return (
         <main className="flex flex-col items-center justify-between min-h-screen p-24">
             <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
@@ -28,7 +28,7 @@ export default function Home() {
             </div>
 
             <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full  before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3  after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-['']  before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-                <h1 className="text-4xl font-semibold lg:text-6xl">Home</h1>
+                <h1 className="text-4xl lg:text-6xl">About</h1>
             </div>
 
             <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
